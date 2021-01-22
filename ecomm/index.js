292caf1de06560b894b3051a0d4a43553f5fc82d
YushunChen <56765896+YushunChen@users.noreply.github.com>
2021-01-21 18:45:39 -0600

    Update

--- ecomm/index.js
+++ ecomm/index.js
@@ -1,43 +1,87 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieSession = require('cookie-session');
+const usersRepo = require('./repositories/users');
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  cookieSession({
+    keys: ['lkasld235j']
+  })
+);
 
-app.get("/", (req, res) => {
+app.get('/signup', (req, res) => {
   res.send(`
     <div>
-        <form method="POST">
-            <input name="email" placeholder="email" />
-            <input name="password" placeholder="password" />
-            <input name="passwordConfirmation" placeholder="password confirmation" />
-            <button>Sign Up</button>
-        </form>
+      Your id is: ${req.session.userId}
+      <form method="POST">
+        <input name="email" placeholder="email" />
+        <input name="password" placeholder="password" />
+        <input name="passwordConfirmation" placeholder="password confirmation" />
+        <button>Sign Up</button>
+      </form>
     </div>
   `);
 });
 
-// const bodyParser = (req, res, next) => {
-//   if (req.method === "POST") {
-//     req.on("data", (data) => {
-//       const parsed = data.toString("utf8").split("&");
-//       const formData = {};
-//       for (let pair of parsed) {
-//         const [key, value] = pair.split("=");
-//         formData[key] = value;
-//       }
-//       req.body = formData;
-//       next();
-//     });
-//   } else {
-//     next();
-//   }
-// };
+app.post('/signup', async (req, res) => {
+  const { email, password, passwordConfirmation } = req.body;
 
-app.post("/", (req, res) => {
-  console.log(req.body);
-  res.send("Account created!");
+  const existingUser = await usersRepo.getOneBy({ email });
+  if (existingUser) {
+    return res.send('Email in use');
+  }
+
+  if (password !== passwordConfirmation) {
+    return res.send('Passwords must match');
+  }
+
+  // Create a user in our user repo to represent this person
+  const user = await usersRepo.create({ email, password });
+
+  // Store the id of that user inside the users cookie
+  req.session.userId = user.id;
+
+  res.send('Account created!!!');
 });
+
+app.get('/signout', (req, res) => {
+  req.session = null;
+  res.send('You are logged out');
+});
+
+app.get('/signin', (req, res) => {
+  res.send(`
+    <div>
+      <form method="POST">
+        <input name="email" placeholder="email" />
+        <input name="password" placeholder="password" />
+        <button>Sign In</button>
+      </form>
+    </div>
+  `);
+});
+
+app.post('/signin', async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await usersRepo.getOneBy({ email });
+
+  if (!user) {
+    return res.send('Email not found');
+  }
+
+  if (user.password !== password) {
+    return res.send('Invalid password');
+  }
+
+  req.session.userId = user.id;
+
+  res.send('You are signed in!!!');
+});
+
 app.listen(3000, () => {
-  console.log("listening");
+  console.log('Listening');
 });
